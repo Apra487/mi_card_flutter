6b1e9435f3f55b9d13b83f0522bfc286a8ac2c79
Apratim Mahata <apra487@gmail.com>
2020-12-25 19:13:01 +0530

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,98 @@
+// import 'dart:math';
+// import 'dart:ui';
+
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 55,
+                // backgroundColor: Colors.black,
+                backgroundImage: AssetImage(
+                    'images/random-girl.jpeg'),
+              ),
+              Text(
+                'Aida',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 38,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico',
+                    letterSpacing: 2.8),
+              ),
+              Text(
+                'A RANDOM GIRL',
+                style: TextStyle(
+                    color: Colors.teal.shade100,
+                    letterSpacing: 1.5,
+                    // fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+              SizedBox(
+                height: 20,
+                width:190,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                // margin: EdgeInsets.symmetric(vertical: 10, horizontal: 1),
+                child: Column(
+                  children: [
+                    Card(
+                      margin: EdgeInsets.fromLTRB(29, 5, 29, 5),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.phone,
+                          size: 25,
+                          color: Colors.teal,
+                        ),
+                        title: Text(
+                          '+91 867 452 343',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.teal[900],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 29),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.mail,
+                          size: 25,
+                          color: Colors.teal,
+                        ),
+                        title: Text(
+                          'dummy@gmail.com',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.teal[900],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
